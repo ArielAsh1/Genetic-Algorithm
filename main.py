6d6978190f2739f671af7941d16fcc705076f6ec
ariel ashkenazy <ariel.ashkenazy@gmail.com>
2023-05-20 11:47:59 +0300

    created read function

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 # ex2
-# checkww
 
 import random
 import string
 
+def read_files():
+    # Load the word list, and the letter and digraph frequencies
+    with open('dict.txt', 'r') as f:
+        word_list = set(line.strip() for line in f)
+        # print(word_list)
+
+    # Create a dictionary where the key is the lowercase letter and the value is the frequency as a float.
+    with open('Letter_Freq.txt', 'r') as f:
+        letter_freqs = {line.strip().split('\t')[1].lower(): float(line.strip().split('\t')[0]) for line in f}
+        # print((letter_freqs))
+
+    # a dictionary mapping lowercase letter pairs to their frequencies
+    # the key is the letters pair and the value is the frequency as a float.
+    with open('Letter2_Freq.txt', 'r') as f:
+        digraph_freqs = {}
+        for line in f:
+            # only proccess lines that contain a tab character
+            if "\t" in line:
+                print(line)
+                digraph_freqs[line.strip().split('\t')[1].lower()] = float(line.strip().split('\t')[0])
+        print(digraph_freqs)
 
 def generate_permutations(starting_population):
     alphabet = list(string.ascii_lowercase)
@@ -18,6 +38,7 @@ def generate_permutations(starting_population):
 
 
 if __name__ == '__main__':
-    perm = generate_permutations(10)
-    for x in perm:
-        print(x)
+    read_files()
+    # perm = generate_permutations(10)
+    # for x in perm:
+    #     print(x)
