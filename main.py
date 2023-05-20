1d7668e08f16d7c94f6cbe440b9318a1c8e93896
OrrBav <orrbavly@gmail.com>
2023-05-20 11:06:38 +0300

    generated starting dictionaries permutations

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # ex2
 # checkww
+
+import random
+import string
+
+
+def generate_permutations(starting_population):
+    alphabet = list(string.ascii_lowercase)
+    permutations = []
+
+    for _ in range(starting_population):
+        random.shuffle(alphabet)
+        permutation = {letter: substitute for letter, substitute in zip(string.ascii_lowercase, alphabet)}
+        permutations.append(permutation)
+
+    return permutations
+
+
+if __name__ == '__main__':
+    perm = generate_permutations(10)
+    for x in perm:
+        print(x)
